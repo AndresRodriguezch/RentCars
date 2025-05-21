d256fbb15a949c355ba9ab7069a2cac07c658555
Alejandro1915 <josemoya1411@gmail.com>
2025-05-20 22:58:35 -0500

    Creacion de  la funcion index el cual pasa la variable vehiculos a la vista Home.

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\vehiculo;
 
 class VehiculoController extends Controller
 {
-    //
+    // Metodo que se ejecuta cuando se accede a la ruta /home
+    public function index()
+    {
+        // Obtiene todos los vehiculos junto con sus relaciones tipoVehiculo y propietario
+        $vehiculos = vehiculo::with(['tipoVehiculo', 'propietario'])->get();
+
+        // Retorna la vista 'Home.home' y le pasa la variable $vehiculos
+        return view('Home.home', compact('vehiculos')); 
+    }
 }
