5556c8a11f8cf00c1a880818dfd98367bdac97e4
Alejandro1915 <josemoya1411@gmail.com>
2025-05-20 23:07:14 -0500

    Inserta registros de prueba en la tabla propietarios desde el seeder.

--- database/seeders/PropietarioSeeder.php
+++ database/seeders/PropietarioSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\propietario;
 
 class PropietarioSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Asegúrate de que existan estos usuarios en la tabla 'usuarios'
+        $propietarios = [
+            ['id_usuario' => 1],
+            ['id_usuario' => 2],
+            ['id_usuario' => 3],
+        ];
+
+        foreach ($propietarios as $p) {
+            propietario::create($p);
+        }
     }
 }
